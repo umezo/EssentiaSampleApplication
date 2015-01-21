e176853e5497ce84b23749dbb3d42398e53c0db9
umezo <umezo@tsuyabu.in>
2015-01-21 15:18:55 +0900

    beat data

---
+++ essentiaScript/example_beattrackermultifeature.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2006-2013  Music Technology Group - Universitat Pompeu Fabra
+#
+# This file is part of Essentia
+#
+# Essentia is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation (FSF), either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the Affero GNU General Public License
+# version 3 along with this program. If not, see http://www.gnu.org/licenses/
+
+
+import json
+import sys
+from essentia.standard import *
+
+
+# In this example we are going to look at how to perform beat tracking
+# and mark extractred beats on the audio using the AudioOnsetsMarker algorithm.
+
+
+# we're going to work with an input and output files specified as an argument in the command line
+try:
+    input_filename = sys.argv[1]
+except:
+    print "usage:", sys.argv[0], "<input-audiofile> <output-audiofile>"
+    sys.exit()
+
+# don't forget, we can actually instantiate and call an algorithm on the same line!
+audio = MonoLoader(filename = input_filename)()
+
+# compute beat positions
+bt = BeatTrackerMultiFeature()
+beats, _ = bt(audio)
+
+jsonBeats = [];
+for beat in beats:
+  jsonBeats.append(float(beat))
+
+print json.dumps(jsonBeats)
